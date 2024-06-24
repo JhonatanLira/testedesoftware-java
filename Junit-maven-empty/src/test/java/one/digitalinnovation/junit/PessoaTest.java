4926ba9d44b3ffb1edea6af9cff396d5a1da7e79
jhonatan <jhonatanlira@gmail.com>
2024-06-24 06:54:47 -0300

    O básico para testar

--- Junit-maven-empty/src/test/java/one/digitalinnovation/junit/PessoaTest.java
+++ Junit-maven-empty/src/test/java/one/digitalinnovation/junit/PessoaTest.java
@@ -9,6 +9,21 @@ public class PessoaTest {
     @Test
     public void validarCalculoIdade() {
         Pessoa pessoa = new Pessoa("Julia", LocalDate.of(2022, 1, 1));
-        Assertions.assertEquals(2,pessoa.getIdade());
+        Assertions.assertEquals(2, pessoa.getIdade());
+    }
+/*
+    @Test
+    void deveCalcularIdadeCorretamente() {
+        Pessoa jessica = new Pessoa("Jessica", LocalDate.of(2000, 1, 1));
+        Assertions.assertEquals(24, jessica.getIdade());
+    }
+*/
+    @Test
+    void deveRetornarSeEhMaiorDeIdade() {
+        Pessoa jessica = new Pessoa("Jessica", LocalDate.of(2000, 1, 1));
+        Assertions.assertTrue(jessica.eMaiorDeIdade());
+
+        Pessoa joao = new Pessoa("João",LocalDate.now());
+        Assertions.assertFalse(joao.eMaiorDeIdade());
     }
 }
